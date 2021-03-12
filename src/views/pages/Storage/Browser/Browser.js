5e5bb0cec038868f33304c56a15c98472d969f36
Tuong Nguyen <tuongn09@gmail.com>
2021-03-12 16:23:37 +0700

    browser component finished ui

--- src/views/pages/Storage/Browser/Browser.js
+++ src/views/pages/Storage/Browser/Browser.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   AppBar,
   Toolbar,
@@ -16,20 +16,52 @@ import {
   TablePagination,
   Checkbox,
   TableSortLabel,
+  Slide,
 } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import DeleteIcon from "@material-ui/icons/Delete";
 import MoreIcon from "@material-ui/icons/MoreVert";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import PublishIcon from "@material-ui/icons/Publish";
+import CreateNewFolderIcon from "@material-ui/icons/CreateNewFolder";
+import ShareIcon from "@material-ui/icons/Share";
+import EditIcon from "@material-ui/icons/Edit";
 
 import "./style.css";
 
-const createData = (name, accessMode) => {
+const createBucketData = (name, accessMode) => {
   return { name, accessMode };
 };
 
-const rows = [
-  createData("Bucket #01", "Public"),
-  createData("Bucket #02", "Private"),
+const createBucketItemData = (name, size, lastModified, accessMode) => {
+  return { name, size, lastModified, accessMode };
+};
+
+const bucketRows = [
+  createBucketData("Bucket #01", "Public"),
+  createBucketData("Bucket #02", "Private"),
+];
+
+const bucketItemRows = [
+  createBucketItemData("File #01", "1.1 KB", "2/12/2020 03:51:16 PM", "Public"),
+  createBucketItemData(
+    "Folder #01",
+    "127.9 MB",
+    "01/07/2020 01:05:51 PM",
+    "Private"
+  ),
+  createBucketItemData(
+    "File #02",
+    "1.1 KB",
+    "09/28/2020 03:09:38 AM",
+    "Private"
+  ),
+  createBucketItemData(
+    "File #03",
+    "1.1KB",
+    "01/14/2020 02:36:00 PM",
+    "Private"
+  ),
 ];
 
 const descendingComparator = (a, b, orderBy) => {
@@ -58,8 +90,31 @@ const stableSort = (array, comparator) => {
   return stabilizedThis.map((el) => el[0]);
 };
 
-const headCells = [
+const bucketHeadCells = [
   { id: "name", numeric: false, disablePadding: true, label: "Name" },
+  {
+    id: "accessMode",
+    numeric: false,
+    disablePadding: false,
+    label: "Access Mode",
+  },
+  {
+    id: "empty",
+    numeric: false,
+    disablePadding: false,
+    label: "",
+  },
+];
+
+const bucketItemHeadCells = [
+  { id: "name", numeric: false, disablePadding: true, label: "Name" },
+  { id: "size", numeric: false, disablePadding: false, label: "Size" },
+  {
+    id: "lastModified",
+    numeric: false,
+    disablePadding: false,
+    label: "Last modified",
+  },
   {
     id: "accessMode",
     numeric: false,
@@ -82,6 +137,7 @@ const EnhancedTableHead = (props) => {
     numSelected,
     rowCount,
     onRequestSort,
+    headCells,
   } = props;
   const createSortHandler = (property) => (event) => {
     onRequestSort(event, property);
@@ -101,7 +157,7 @@ const EnhancedTableHead = (props) => {
         {headCells.map((headCell) => (
           <TableCell
             key={headCell.id}
-            style={{ width: "90%" }}
+            style={headCell.style}
             align="left"
             padding={headCell.disablePadding ? "none" : "default"}
             sortDirection={orderBy === headCell.id ? order : false}
@@ -120,7 +176,92 @@ const EnhancedTableHead = (props) => {
   );
 };
 
-const EnhancedTable = ({ selected, setSelected }) => {
+const BucketContainer = ({ items, onItemClick, visibility, ...props }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [selected, setSelected] = useState([]);
+  const isMenuOpen = Boolean(anchorEl);
+
+  const menuId = "mobile-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMenuOpen}
+      onClose={() => setAnchorEl(null)}
+    >
+      <MenuItem>
+        <Button startIcon={<AddIcon />}>Create bucket</Button>
+      </MenuItem>
+      <MenuItem>
+        <Button
+          startIcon={<DeleteIcon />}
+          disabled={selected.length !== 0 ? false : true}
+        >
+          Delete bucket
+        </Button>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <Paper
+      style={{
+        position: "relative",
+        width: "100%",
+      }}
+      visibility={visibility}
+    >
+      <AppBar
+        position="static"
+        style={{ backgroundColor: "white", color: "black" }}
+      >
+        <Toolbar variant="dense">
+          <h3 style={{ marginRight: "20px" }}>Bucket</h3>
+          <div className="browser-appbar-button-group">
+            <Button startIcon={<AddIcon />}>Create bucket</Button>
+            <Button
+              startIcon={<DeleteIcon />}
+              disabled={selected.length !== 0 ? false : true}
+            >
+              Delete bucket
+            </Button>
+          </div>
+          <div style={{ flexGrow: "1" }}></div>
+          <div className="browser-appbar-mobile-menu">
+            <IconButton
+              aria-label="show more"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={(e) => setAnchorEl(e.currentTarget)}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <BucketTable
+        headCells={bucketHeadCells}
+        items={items}
+        selected={selected}
+        setSelected={setSelected}
+        onItemClick={onItemClick}
+      />
+      {renderMenu}
+      {props.children}
+    </Paper>
+  );
+};
+
+const BucketTable = ({
+  selected,
+  setSelected,
+  headCells,
+  items,
+  onItemClick,
+}) => {
   const [order, setOrder] = useState("asc");
   const [orderBy, setOrderBy] = useState("name");
   const [page, setPage] = useState(0);
@@ -134,7 +275,7 @@ const EnhancedTable = ({ selected, setSelected }) => {
 
   const handleSelectAllClick = (e) => {
     if (e.target.checked) {
-      const newSelecteds = rows.map((n) => n.name);
+      const newSelecteds = bucketRows.map((n) => n.name);
       setSelected(newSelecteds);
       return;
     }
@@ -172,7 +313,7 @@ const EnhancedTable = ({ selected, setSelected }) => {
   const isSelected = (name) => selected.indexOf(name) !== -1;
 
   const emptyRows =
-    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+    rowsPerPage - Math.min(rowsPerPage, bucketRows.length - page * rowsPerPage);
 
   return (
     <Paper>
@@ -184,10 +325,11 @@ const EnhancedTable = ({ selected, setSelected }) => {
             orderBy={orderBy}
             onSelectedAllClick={handleSelectAllClick}
             onRequestSort={handleRequestSort}
-            rowCount={rows.length}
+            rowCount={bucketRows.length}
+            headCells={headCells}
           />
           <TableBody>
-            {stableSort(rows, getComparator(order, orderBy))
+            {stableSort(items, getComparator(order, orderBy))
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               .map((row, index) => {
                 const isItemSelected = isSelected(row.name);
@@ -211,7 +353,7 @@ const EnhancedTable = ({ selected, setSelected }) => {
                     <TableCell
                       component="th"
                       id={labelId}
-                      onClick={() => alert(`${row.name} clicked!`)}
+                      onClick={() => onItemClick(row.name)}
                       scope="row"
                       padding="none"
                     >
@@ -219,7 +361,7 @@ const EnhancedTable = ({ selected, setSelected }) => {
                     </TableCell>
                     <TableCell
                       align="left"
-                      onClick={() => alert(`${row.name} clicked!`)}
+                      onClick={() => onItemClick(row.name)}
                     >
                       {row.accessMode}
                     </TableCell>
@@ -242,7 +384,7 @@ const EnhancedTable = ({ selected, setSelected }) => {
       <TablePagination
         rowsPerPageOptions={[5, 10, 15]}
         component="div"
-        count={rows.length}
+        count={bucketRows.length}
         rowsPerPage={rowsPerPage}
         page={page}
         onChangePage={handleChangePage}
@@ -252,7 +394,7 @@ const EnhancedTable = ({ selected, setSelected }) => {
   );
 };
 
-const Browser = (props) => {
+const BucketItemsContainer = ({ title, items, onBack, onItemClick }) => {
   const [anchorEl, setAnchorEl] = useState(null);
   const [selected, setSelected] = useState([]);
   const isMenuOpen = Boolean(anchorEl);
@@ -282,41 +424,238 @@ const Browser = (props) => {
   );
 
   return (
-    <>
-      <AppBar
-        position="static"
-        style={{ backgroundColor: "white", color: "black" }}
-      >
-        <Toolbar variant="dense">
-          <h3 style={{ marginRight: "20px" }}>Bucket</h3>
-          <div className="browser-appbar-button-group">
-            <Button startIcon={<AddIcon />}>Create bucket</Button>
-            <Button
-              startIcon={<DeleteIcon />}
-              disabled={selected.length !== 0 ? false : true}
-            >
-              Delete bucket
-            </Button>
-          </div>
-          <div style={{ flexGrow: "1" }}></div>
-          <div className="browser-appbar-mobile-menu">
-            <IconButton
-              aria-label="show more"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={(e) => setAnchorEl(e.currentTarget)}
-              color="inherit"
-            >
-              <MoreIcon />
+    <Slide
+      in={items !== null ? true : false}
+      direction="left"
+      mountOnEnter
+      unmountOnExit
+    >
+      <Paper style={{ position: "absolute", width: "inherit", top: "0" }}>
+        <AppBar
+          position="static"
+          style={{ backgroundColor: "white", color: "black" }}
+        >
+          <Toolbar variant="dense">
+            <IconButton onClick={() => onBack(null)}>
+              <ArrowBackIcon />
             </IconButton>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <div style={{ width: "100%" }}>
-        <EnhancedTable selected={selected} setSelected={setSelected} />
-      </div>
-      {renderMenu}
-    </>
+            <h3 style={{ marginRight: "20px" }}>{title}</h3>
+            <div className="browser-appbar-button-group">
+              <Button startIcon={<PublishIcon />}>Upload file</Button>
+              <Button startIcon={<PublishIcon />}>Upload folder</Button>
+              <Button startIcon={<CreateNewFolderIcon />}>Create folder</Button>
+            </div>
+            <div style={{ flexGrow: "1" }}></div>
+            <div className="browser-appbar-button-group">
+              <Button startIcon={<ShareIcon />}>Share</Button>
+              <Button startIcon={<EditIcon />}>Edit bucket</Button>
+              <Button startIcon={<DeleteIcon />}>Delete</Button>
+            </div>
+            <div className="browser-appbar-mobile-menu">
+              <IconButton
+                aria-label="show more"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={(e) => setAnchorEl(e.currentTarget)}
+                color="inherit"
+              >
+                <MoreIcon />
+              </IconButton>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <BucketItemTable
+          headCells={bucketItemHeadCells}
+          selected={selected}
+          setSelected={setSelected}
+          items={items || []}
+          onItemClick={onItemClick}
+        />
+        {renderMenu}
+      </Paper>
+    </Slide>
+  );
+};
+
+const BucketItemTable = ({
+  selected,
+  setSelected,
+  headCells,
+  items,
+  onItemClick,
+}) => {
+  const [order, setOrder] = useState("asc");
+  const [orderBy, setOrderBy] = useState("name");
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (e) => {
+    if (e.target.checked) {
+      const newSelecteds = bucketItemRows.map((n) => n.name);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleItemCheckboxClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const isSelected = (name) => selected.indexOf(name) !== -1;
+
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, bucketRows.length - page * rowsPerPage);
+
+  return (
+    <Paper>
+      <TableContainer>
+        <Table>
+          <EnhancedTableHead
+            numSelected={selected.length}
+            order={order}
+            orderBy={orderBy}
+            onSelectedAllClick={handleSelectAllClick}
+            onRequestSort={handleRequestSort}
+            rowCount={bucketItemRows.length}
+            headCells={headCells}
+          />
+          <TableBody>
+            {stableSort(items, getComparator(order, orderBy))
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row, index) => {
+                const isItemSelected = isSelected(row.name);
+                const labelId = `enhanced-table-checkbox-${index}`;
+
+                return (
+                  <TableRow
+                    hover
+                    aria-checked={isItemSelected}
+                    tabIndex={-1}
+                    key={row.name}
+                    selected={isItemSelected}
+                  >
+                    <TableCell padding="checkbox">
+                      <Checkbox
+                        checked={isItemSelected}
+                        onChange={(e) => handleItemCheckboxClick(e, row.name)}
+                        inputProps={{ "aria-labelledby": labelId }}
+                      />
+                    </TableCell>
+                    <TableCell
+                      component="th"
+                      id={labelId}
+                      scope="row"
+                      padding="none"
+                      onClick={() => onItemClick(row.name)}
+                    >
+                      {row.name}
+                    </TableCell>
+                    <TableCell
+                      align="left"
+                      onClick={() => onItemClick(row.name)}
+                    >
+                      {row.size}
+                    </TableCell>
+                    <TableCell
+                      align="left"
+                      onClick={() => onItemClick(row.name)}
+                    >
+                      {row.lastModified}
+                    </TableCell>
+                    <TableCell
+                      align="left"
+                      onClick={() => onItemClick(row.name)}
+                    >
+                      {row.accessMode}
+                    </TableCell>
+                    <TableCell align="right">
+                      <IconButton>
+                        <MoreIcon />
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={6} />
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 15]}
+        component="div"
+        count={bucketItemRows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+};
+
+const Browser = () => {
+  const [buckets, setBuckets] = useState(bucketRows);
+  const [bucketItems, setBucketItems] = useState(null);
+  // const [bucketItemSelected, setBucketItemSelected] = useState({});
+
+  const [bucketSelected, setBucketSelected] = useState(null);
+
+  useEffect(() => {
+    if (bucketSelected === null) {
+      setBucketItems(null);
+      return;
+    }
+    setBucketItems(bucketItemRows);
+  }, [bucketSelected]);
+
+  return (
+    <BucketContainer
+      items={buckets}
+      onItemClick={(name) => setBucketSelected(name)}
+      visibility={bucketSelected !== null ? "hidden" : "visible"}
+    >
+      <BucketItemsContainer
+        title={bucketSelected}
+        items={bucketItems}
+        onBack={() => setBucketSelected(null)}
+        onItemClick={(name) => alert(name)}
+      />
+    </BucketContainer>
   );
 };
 
