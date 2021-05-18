421e6c6a0354d8e1e56d17d0105bfdda00460cf4
Tuong Nguyen <tuongn09@gmail.com>
2021-05-18 08:41:13 +0700

    add unit and total bandwidth usage

--- src/views/pages/Admin/AdminDashboard.js
+++ src/views/pages/Admin/AdminDashboard.js
@@ -5,6 +5,14 @@ import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import AppBar from "@material-ui/core/AppBar";
 import AdminDrawer from "../../components/AdminDrawer";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
 import LogTable from "../../components/LogTable/LogTable";
 
 const AdminDashboard = (props) => {
@@ -14,19 +22,48 @@ const AdminDashboard = (props) => {
     setValue(newValue);
   };
 
+  const CustomTooltip = ({ payload, active }, ...props) => {
+    if (active) {
+      console.log(payload);
+      return <p>{`${payload[0].value} kB`}</p>;
+    }
+    return null;
+  };
+
+  const renderLineChart = (data, width = 1000, height = 500) => (
+    <LineChart
+      width={width}
+      height={height}
+      data={data}
+      margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+    >
+      <Line type="monotone" dataKey="bandwidth" stroke="#8884d8" />
+      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+      <XAxis dataKey="day" />
+      <YAxis />
+      <Tooltip content={<CustomTooltip />} />
+    </LineChart>
+  );
+
   return (
     <AdminDrawer title="Dashboard">
       <AppBar
         position="static"
-        style={{ backgroundColor: "white", color: "black" }}>
+        style={{ backgroundColor: "white", color: "black" }}
+      >
         <Tabs value={value} onChange={handleChange}>
-          <Tab style={{ outline: 'none' }} className="outline-none" label="Monitor Request Log" {...a11yProps(0)} />
+          <Tab
+            style={{ outline: "none" }}
+            className="outline-none"
+            label="Monitor Request Log"
+            {...a11yProps(0)}
+          />
           {/* <Tab style={{ outline: 'none' }} className="focus:outline-none" label="Access Key Log" {...a11yProps(1)} />
           <Tab style={{ outline: 'none' }} className="focus:outline-none" label="Key Pairs Log" {...a11yProps(2)} /> */}
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
-        <LogTable/>
+        <LogTable />
       </TabPanel>
     </AdminDrawer>
   );
--- src/views/pages/Dashboard/Dashboard.js
+++ src/views/pages/Dashboard/Dashboard.js
@@ -16,50 +16,65 @@ import {
   Tooltip,
 } from "recharts";
 import store from "../../../store/store";
-import { getTotalBandwidth } from "../../../store/user/bandwidthReport";
+import {
+  getMonthUsageBandwidth,
+  getTotalUsageBandwidth,
+} from "../../../store/user/bandwidthReport";
 
 const Dashboard = (props) => {
   const [value, setValue] = React.useState(0);
 
   React.useEffect(() => {
-    store.dispatch(getTotalBandwidth({ authToken: props.authToken }));
+    store.dispatch(getMonthUsageBandwidth({ authToken: props.authToken }));
+    store.dispatch(getTotalUsageBandwidth({ authToken: props.authToken }));
   }, []);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  const renderLineChart = (data, width = 600, height = 300) => (
+  const CustomTooltip = ({ payload, active }, ...props) => {
+    if (active) {
+      console.log(payload);
+      return <p>{`${payload[0].value} kB`}</p>;
+    }
+    return null;
+  };
+
+  const renderLineChart = (data, width = 1000, height = 500) => (
     <LineChart
       width={width}
       height={height}
       data={data}
       margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
     >
-      <Line type='monotone' dataKey='uv' stroke='#8884d8' />
-      <CartesianGrid stroke='#ccc' strokeDasharray='5 5' />
-      <XAxis dataKey='name' />
+      <Line type="monotone" dataKey="bandwidth" stroke="#8884d8" />
+      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+      <XAxis dataKey="day" />
       <YAxis />
-      <Tooltip />
+      <Tooltip content={<CustomTooltip />} />
     </LineChart>
   );
 
   return (
-    <PersistentDrawer title='Dashboard'>
+    <PersistentDrawer title="Dashboard">
       <AppBar
-        position='static'
+        position="static"
         style={{ backgroundColor: "white", color: "black" }}
       >
         <Tabs value={value} onChange={handleChange}>
           <Tab
-            className='focus:outline-none'
-            label='Dashboard'
+            className="focus:outline-none"
+            label="Dashboard"
             {...a11yProps(0)}
           />
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
-        {renderLineChart(props.data)}
+        <div>
+          {renderLineChart(props.data)}
+          <p>Total bandwidth usage in this month: {props.total} kB</p>
+        </div>
       </TabPanel>
     </PersistentDrawer>
   );
@@ -70,7 +85,7 @@ function TabPanel(props) {
 
   return (
     <div
-      role='tabpanel'
+      role="tabpanel"
       hidden={value !== index}
       id={`simple-tabpanel-${index}`}
       aria-labelledby={`simple-tab-${index}`}
@@ -100,10 +115,11 @@ function a11yProps(index) {
 
 const mapStateToProps = (state) => {
   const data = state.bandwidthReport.data;
+  const total = state.bandwidthReport.total;
   const loading = state.bandwidthReport.loading;
   const err = state.bandwidthReport.err;
   const authToken = state.authen.authToken;
-  return { data, loading, err, authToken };
+  return { data, total, loading, err, authToken };
 };
 
 export default connect(mapStateToProps)(Dashboard);
